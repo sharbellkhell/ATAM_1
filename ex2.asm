d4863568d5d6a43e3ad331558d757c7d439834ff
sharbellkhell <88714465+sharbellkhell@users.noreply.github.com>
2022-11-21 22:18:54 +0200

    Ex2 - Not working yet (SIGSEV at line 16)

--- ex2.asm
+++ ex2.asm
@@ -2,4 +2,29 @@
 
 .section .text
 _start:
-#your code here
+    mov source, %rax
+    mov dest, %rbx
+    xor %rcx, %rcx
+    movl num, %edx
+    test %rdx,%rdx
+    jz END
+    cmp %rcx, %rdx  # 0 <= num
+    jge POSITIVE
+    lea (%rbx,%rdx, 8), %rbx
+NEGATIVE:
+    movb (%rax), %cl
+    movb %cl, (%rbx)
+    add $8, %rax
+    sub $8, %rbx
+    dec %rdx
+    jnz NEGATIVE
+    jmp END
+POSITIVE:
+    movb (%rax), %cl
+    movb %cl, (%rbx)
+    add $8, %rax
+    add $8, %rbx
+    dec %rdx
+    jnz POSITIVE
+
+END: nop
