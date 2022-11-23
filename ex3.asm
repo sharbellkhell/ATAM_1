ed3161b50774866deea91fcbbf876296703bbbab
Ryuketsukami <Ryuketsukami@gmail.com>
2022-11-23 22:30:33 +0200

    Update ex3.asm

--- ex3.asm
+++ ex3.asm
@@ -2,4 +2,48 @@
 
 .section .text
 _start:
+    movq %rsp, %rbp #for correct debugging
+    xor %rax, %rax # index1
+    xor %rbx, %rbx # index2
+    xor %ecx, %ecx # last added number
+    xor %rdx, %rdx # Index of merged array
+    xor %r8d, %r8d
+    xor %r9d, %r9d
+    
+    lea array1, %rax
+    lea array2, %rbx
+    lea mergedArray, %rdx
+
+LOOP_HW1: 
+    mov (%rax), %r8d
+    mov (%rbx),%r9d
+    
+    test %r8d, %r8d
+    jnz CONTINUE
+    test %r9d, %r9d
+    jz END
+
+CONTINUE:
+    cmp %r8d, %r9d
+    ja A2_GREAT	# array1(index1) < array2(index2)
+
+A1_GREAT_EQUAL:
+    add $4, %rax
+    cmp %r8d, %ecx # check if current value and last value are equal
+    je LOOP_HW1
+    mov %r8d, %ecx
+    mov %r8d, (%rdx)
+    add $4, %rdx
+    jmp LOOP_HW1
+A2_GREAT:
+    add $4, %rbx
+    cmp %r9d, %ecx #check if current value and last value are equal
+    je LOOP_HW1
+    mov %r9d, %ecx
+    mov %r9d, (%rdx)
+    add $4, %rdx
+    jmp LOOP_HW1
+
+END:
+    movl $0x0, (%rdx)
 #your code here
