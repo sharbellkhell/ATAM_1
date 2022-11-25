4d3ab7d1d5306a1b5e3b449d223d6ae3974e26c6
Ryuketsukami <Ryuketsukami@gmail.com>
2022-11-25 22:23:36 +0200

    Update ex2.asm

--- ex2.asm
+++ ex2.asm
@@ -1,7 +1,7 @@
+.global _start
 
-.text
-.global main
-main:
+.section .text
+_start:
     movq %rsp, %rbp #for correct debugging
     mov source, %rax
     mov dest, %rbx
@@ -9,40 +9,40 @@ main:
     xor %rcx, %rcx
     movl num, %edx
     test %edx,%edx
-    jz END
+    jz END_HW1
     cmp %ecx, %edx  # 0 <= num
-    jle NEGATIVE_START
+    jle NEGATIVE_START_HW1
     lea (%rbx,%rdx, 1), %rbx #to be edited
     dec %rbx #to be edited
-POSITIVE_PUSH:
+POSITIVE_PUSH_HW1:
     movb (%rax), %cl
     pushq %rcx
     add $1, %rax
     dec %rdx
-    jnz POSITIVE_PUSH
+    jnz POSITIVE_PUSH_HW1
     movl num, %edx
-POSITIVE_POP:
+POSITIVE_POP_HW1:
     popq %rcx
     movb %cl, (%rbx)
     dec %rbx
     dec %rdx
-    jnz POSITIVE_POP
-    jmp END
-NEGATIVE_START:
+    jnz POSITIVE_POP_HW1
+    jmp END_HW1
+NEGATIVE_START_HW1:
     neg %edx
-NEGATIVE_PUSH:
+NEGATIVE_PUSH_HW1:
     movb (%rax), %cl
     pushq %rcx
     add $1, %rax
     dec %rdx
-    jnz NEGATIVE_PUSH
+    jnz NEGATIVE_PUSH_HW1
     movl num, %edx
     neg %edx
-NEGATIVE_POP:
+NEGATIVE_POP_HW1:
     popq %rcx
     movb %cl, (%rbx)
     add $1, %rbx
     dec %rdx
-    jnz NEGATIVE_POP
+    jnz NEGATIVE_POP_HW1
     
-END:
+END_HW1:
